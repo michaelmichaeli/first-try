c7d0104f7a1535f2dedc69563b18b0a75811619f
michaelmichaeli888 <michaelmichaeli888@gmail.com>
2021-04-13 14:17:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles/main.scss";
+
+function App() {
+	return (
+		<div className="App">
+			<div className="main-container">
+				<header>header</header>
+				<main>main</main>
+				<footer>footer</footer>
+			</div>
+		</div>
+	);
+}
+
+export default App;
