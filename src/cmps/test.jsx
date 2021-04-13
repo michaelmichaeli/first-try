b5c65bd6f2e9d86aff2d33084f5e90fe9f08fb94
michaelmichaeli888 <michaelmichaeli888@gmail.com>
2021-04-13 19:23:02 +0300

    again with test file of converting class to hook

---
+++ src/cmps/test.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import userService from './services/userService.js'
+
+
+function App() {
+
+    const [ user, setUser ] = useState;
+    const [ fullName, setFullName ] = useState;
+
+    useEffect(() => {
+        loadUsers()
+    }, []);
+
+
+    const loadUsers = async () => {
+        const users = await userService.query()
+        setUser({ users })
+    }
+
+    const showName = () => {
+        alert(fullName);
+    };
+
+    const handleNameInput = e => {
+        setFullName( e.target.value );
+    };
+
+            return (
+            <div>
+                <h3>This is a Class Component</h3>
+                <h4>{this.state.users.length}</h4>
+                <input
+                    type="text"
+                    onChange={handleNameInput}
+                    value={fullName}
+                    placeholder="Your Name"
+                />
+                <button onClick={showName}>
+                    Alert
+        </button>
+            </div>
+        );
+}
+
+export default App;
